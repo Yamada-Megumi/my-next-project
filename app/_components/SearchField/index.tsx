5337d5def6e1b084ad5695bc07ef0b6ec1a78e9c
jobtraining00t@gmail.com <00T>
2024-09-25 12:24:53 +0900

    7章まで終了

--- app/_components/SearchField/index.tsx
+++ app/_components/SearchField/index.tsx
@@ -1,21 +1,24 @@
 "use client";
 
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import styles from "./index.module.css";
+import { Suspense } from "react";
 
-export default function SearchField() {
+function SearchFieldComponent() {
   const router = useRouter();
+  const searchParams = useSearchParams();
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const q = e.currentTarget.elements.namedItem("q");
     if (q instanceof HTMLInputElement) {
       const params = new URLSearchParams();
-      params.set("q", q.trim());
+      params.set("q", q.value.trim());
       router.push(`/news/search?${params.toString()}`);
     }
   };
+
   return (
     <form onSubmit={handleSubmit} className={styles.form}>
       <label className={styles.search}>
@@ -27,12 +30,21 @@ export default function SearchField() {
           loading="eager"
         />
         <input
-          type="search"
+          type="text"
           name="q"
+          defaultValue={searchParams.get("q") ?? undefined}
           placeholder="キーワードを入力"
           className={styles.searchInput}
         />
       </label>
     </form>
+  );
+}
+
+export default function SearchField() {
+  return (
+    <Suspense>
+      <SearchFieldComponent />
+    </Suspense>
   );
 }
